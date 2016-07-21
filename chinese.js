d59eca8343d8e95a613ad1b37d79aed5767be2c4
Jacob Keen <jaclkeen@gmail.com>
2016-07-21 15:50:26 -0500

    added chinese.js

---
+++ chinese.js
@@ -0,0 +1,30 @@
+var translator = (function (chineseTranslator) {
+ var chinese = {
+  "merry":"圣诞快乐",
+  "christmas": "圣诞节",
+  "happy":"快乐" ,
+  "holidays":"假期",
+  "yule":"圣诞" ,
+  "tide":"潮汐",
+  "greeting":"问候",
+  "to": "给",
+  "the": "的",
+  "and":"而且",
+  "i": "我",
+  "family":"的家庭"
+}
+chineseTranslator.translateToChinese = function(input) {
+ var translateSentence = document.getElementById("result");
+ var translatedChinese = "";
+ var makeArray = input.toLowerCase().split(" ");
+
+ makeArray.forEach(function(element){
+   translatedChinese += chinese[element] + " ";
+  })
+
+ console.log(translatedChinese)
+ translateSentence.value = translatedChinese;
+};
+
+return chineseTranslator;
+})(Translator || {});
