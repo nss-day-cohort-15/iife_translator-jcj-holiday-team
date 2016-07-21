d7d6324a39587d2b793e15fe53d1c5595a3ebe56
Chris Smalley <chrism.smalley@gmail.com>
2016-07-21 11:14:54 -0500

    initial push to spanish branch

---
+++ spanish.js
@@ -0,0 +1,3 @@
+var spanish = {
+"merry" : "feliz", "christmas" : "navidad", "happy" : "contento", "holidays" : "dias de fiesta", "yuletide" : "yuletide", "seasons" : "saludos", "the" : "las", "greetings" : "estación", "and" : "y", "I" : "yo", "your" : "tu"
+}
